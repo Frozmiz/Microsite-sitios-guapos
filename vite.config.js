222389bb5efd5d64487cc600e41c73bf52aa31cc
Alejandro Gonzalez <frozmiz@live.com>
2025-07-01 01:24:11 +0200

    fix: web name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/ConquerBlocks-CSS-P4-Portfolio/",
+  base: "/Microsite-sitios-guapos/",
   build: {
     rollupOptions: {
       input: [
