b4532e22144ec709b00b6bedf45b248cd98a1437
Alejandro Gonzalez <frozmiz@live.com>
2025-07-10 02:22:13 +0200

    chore: update vite.config.js configuration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
       input: [
         "index.html",
         "quienes-somos.html",
-        "destinos.html",
+        "destinos-page.html",
         "blog.html",
         "registro.html",
         "login.html",
